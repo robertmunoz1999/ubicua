cb5922bc9e68371e701d9f10c2c1b621bec523cc
Roberto Muñoz <roberto.munozs@edu.uah.com>
2020-12-08 18:54:10 +0100

    Acabadas las estadísticas a falta de mejorar el diseño

--- UbicuaInterfaz/src/Graficos/Graficar.java
+++ UbicuaInterfaz/src/Graficos/Graficar.java
@@ -19,25 +19,28 @@ public class Graficar {
     
     Connection connection = Conexion.getConnection();
     
-    public DefaultTableModel mostrarCuentas(){
+    public DefaultTableModel getCubeData(int cube){
         DefaultTableModel miModelo = null;
         try{
-            String titulos [] = {"Id", "Nombre"};
-            String dts [] = new String[2];
+            String titulos [] = {"Id cubo", "Referencia data", "Capacidad", "C02", "Metano", "Humo", "Sello temporal"};
+            String dts [] = new String[7];
             miModelo = new DefaultTableModel(null, titulos);
-            String sql = "SELECT * FROM ACCOUNT";
+            String sql = "select * from cube_data_record where id_cube = " + cube;
             PreparedStatement pst = connection.prepareStatement(sql);
             ResultSet rs = pst.executeQuery();
             while(rs.next()){
-                dts[0] = rs.getString("email");
-                dts[1] = rs.getString("password");
+                dts[0] = rs.getString("id_cube");
+                dts[1] = rs.getString("id");
+                dts[2] = rs.getString("capacity");
+                dts[3] = rs.getString("c02");
+                dts[4] = rs.getString("methane");
+                dts[5] = rs.getString("smoke");
+                dts[6] = rs.getString("data_timestamp");
                 miModelo.addRow(dts);
             }
         } catch(Exception ex){
             ex.printStackTrace();
         }
         return miModelo;
-        
     }
-    
 }
