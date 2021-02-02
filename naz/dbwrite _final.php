582ed440939f7f5801fc9e50c3610e915707a240
Roberto Muñoz <roberto.munozs@edu.uah.com>
2021-02-02 22:06:22 +0100

    mend

--- naz/dbwrite _final.php
+++ naz/dbwrite _final.php
@@ -22,9 +22,21 @@ $data = [
     'chat_id' => '@Basur-app',
     'text' => 'La temperatura es demasiado elevada, llame a los bomberos'
 ];
+// Set your Bot ID and Chat ID.
+$telegrambot='1562371383:AAECc-TSUOn4zcAq9pnGf-9XGnO82VTrg6Y';
+$telegramchatid=-1001353859969;
+// Telegram function which you can call
+function telegram($msg) {
+        global $telegrambot,$telegramchatid;
+        $url='https://api.telegram.org/bot'.$telegrambot.'/sendMessage';$data=array('chat_id'=>$telegramchatid,'text'=>$msg);
+        $options=array('http'=>array('method'=>'POST','header'=>"Content-Type:application/x-www-form-urlencoded\r\n",'content'=>http_build_query($data),),);
+        $context=stream_context_create($options);
+        $result=file_get_contents($url,false,$context);
+        return $result;
+}
 
-$response = getSslPage("https://api.telegram.org/bot$apiToken/sendMessage?
-" . http_build_query($data) );
+/*$response = getSslPage("https://api.telegram.org/bot$apiToken/sendMessage?
+" . http_build_query($data) );*/
     
     if(
            !empty($_POST['id_cube']) 
@@ -44,15 +56,17 @@ $response = getSslPage("https://api.telegram.org/bot$apiToken/sendMessage?
         $temperatura = $_POST['temperatura'];
         $resistance1 = $_POST['MQ2_resistencia'];
         $resistance2 = $_POST['MQ135_resistencia'];
-        $response = getSslPage("https://api.telegram.org/bot1562371383:AAECc-TSUOn4zcAq9pnGf-9XGnO82VTrg6Y/sendMessage?chat_id=-1001353859969&text=Hola Mundo" );
-        echo $response;
+        //$response = getSslPage("https://api.telegram.org/bot1562371383:AAECc-TSUOn4zcAq9pnGf-9XGnO82VTrg6Y/sendMessage?chat_id=-1001353859969&text=Hola Mundo" );
+        telegram ("Hola mundo");
+        //echo $response;
+        echo "enviado";
         /*if(temperatura>50){
             sendMsg($msgID,"Alerta, el cubo $id_cube tiene demasiada temperatura",$website)
         }*/
     
     	//insertCubeDataRecord($id_cube, $capacidad, $co2, $metano, $humo, $temperatura, $resistance1, $resistance2);
     }
-function getSslPage($url){
+/*function getSslPage($url){
     $ch = curl_init();
     curl_setopt($ch ,CURLOPT_SSL_VERIFYPEER,FALSE); 
     curl_setopt($ch ,CURLOPT_HEADER, false);
@@ -64,7 +78,14 @@ function getSslPage($url){
     curl_close($ch);
     return $result;
 
-}
+}*/
+
+
+
+
+
+// Function call with your own text or variable
+
 
 function insertCubeDataRecord($id_cube, $capacity, $co2, $methane, $smoke, $temperature, $resistance1, $resistance2){
     $host = "localhost";
